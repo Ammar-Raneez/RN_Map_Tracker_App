ab660484a949bad9386d3addfaac943c355f8223
ammar <ammarraneez@gmail.com>
2022-07-01 21:59:18 +0530

    feat: track list screen

---
+++ track-client/src/screens/TrackListScreen.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useEffect } from 'react';
+import { FlatList, TouchableOpacity } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import { ListItem } from 'react-native-elements';
+
+import { Context as TrackContext } from '../context/TrackContext';
+
+const TrackListScreen = () => {
+  const { state, fetchTracks } = useContext(TrackContext);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchTracks();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('TrackDetail', { _id: item._id })
+              }
+            >
+              <ListItem>
+                <ListItem.Content>
+                  <ListItem.Title>{item.name}</ListItem.Title>
+                </ListItem.Content>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    </>
+  );
+};
+
+TrackListScreen.navigationOptions = {
+  title: 'Tracks',
+};
+
+export default TrackListScreen;
