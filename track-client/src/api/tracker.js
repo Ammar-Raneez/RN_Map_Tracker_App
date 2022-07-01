65231132bb4986a84953f02c84fae369ed71a218
ammar <ammarraneez@gmail.com>
2022-07-01 21:39:57 +0530

    feat: API & mock data

---
+++ track-client/src/api/tracker.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const instance = axios.create({
+  baseURL: 'http://34a8-2402-4000-2280-fae3-589-380f-1722-20da.ngrok.io',
+});
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
