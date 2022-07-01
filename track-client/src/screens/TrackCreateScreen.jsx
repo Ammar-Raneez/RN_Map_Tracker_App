2a02dfeea5a5d8b199723e6056555922de39ee37
ammar <ammarraneez@gmail.com>
2022-07-01 21:59:02 +0530

    feat: track create screem

---
+++ track-client/src/screens/TrackCreateScreen.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useCallback } from 'react';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useIsFocused } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+
+import Map from '../components/Map';
+import TrackForm from '../components/TrackForm';
+import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
+import '../_mockLocation';
+
+const TrackCreateScreen = () => {
+  const isFocused = useIsFocused();
+
+  const {
+    state: { recording },
+    addLocation
+  } = useContext(LocationContext);
+
+  const callback = useCallback(
+    location => {
+      addLocation(location, recording);
+    },
+    [recording]
+  );
+
+  const [err] = useLocation(isFocused || recording, callback);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text h2>Create a Track</Text>
+      <Map />
+      {err ? <Text>Please enable location services</Text> : null}
+      <TrackForm />
+    </SafeAreaView>
+  );
+};
+
+TrackCreateScreen.navigationOptions = {
+  title: 'Add Track',
+  tabBarIcon: <FontAwesome name="plus" size={20} />
+};
+
+export default TrackCreateScreen;
