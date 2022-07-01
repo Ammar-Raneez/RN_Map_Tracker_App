3aa375c05a4bb27d46bde6ce837a1a46af70df57
ammar <ammarraneez@gmail.com>
2022-07-01 21:58:33 +0530

    feat: account screen

---
+++ track-client/src/screens/AccountScreen.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
+
+const AccountScreen = () => {
+  const { signout } = useContext(AuthContext);
+
+  return (
+    <SafeAreaView forceInset={{ top: 'always' }}>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signout} />
+      </Spacer>
+    </SafeAreaView>
+  );
+}
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />
+};
+
+export default AccountScreen;
